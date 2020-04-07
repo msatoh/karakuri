b6b2a325fa3f43a6ab58a1e20abf7eca19975722
msatoh <msatoh@lily.ocn.ne.jp>
2020-04-07 21:48:52 +0900

    マウス動作検出

---
+++ test.py
@@ -0,0 +1,45 @@
+# coding:UTF=8
+
+# from PIL import Image
+# import numpy
+import pdb,sys,time,cv2
+#import tkinter
+#import pyautogui
+
+
+class Mouse():
+	def __init__(self):
+		self.x_hold=0
+		self.y_hold=0
+		self.hold=False
+	def mouse_event(self,event,x,y,flags,param):
+		if event==cv2.EVENT_LBUTTONDOWN:
+			self.x_hold=x
+			self.y_hold=y
+			self.hold=True
+			#cv2.circle(img,(x,y),50,(0,0,255),-1)
+		elif event==cv2.EVENT_MOUSEMOVE and self.x_hold!=0 and self.y_hold!=0:
+			#print(self.x_hold,self.y_hold,x,y)
+			if flags==cv2.EVENT_FLAG_LBUTTON:
+				if x-self.x_hold>10:
+					print("→")
+				elif x-self.x_hold<-10:
+					print("←")
+				elif y-self.y_hold<-10:
+					print("↑")
+				elif y-self.y_hold>10:
+					print("↓")
+
+img=cv2.imread("e6af7755-s.jpg",1)
+cv2.namedWindow("img",cv2.WINDOW_NORMAL)
+
+mouse_t=Mouse()
+
+cv2.setMouseCallback("img",mouse_t.mouse_event)
+
+while(True):
+	cv2.imshow("img",img)
+	if cv2.waitKey(1) & 0xff==ord("q"):
+		break
+
+cv2.destroyAllWindows()
