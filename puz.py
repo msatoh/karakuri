a77268359c7dd0785b7a6be4ca860ebf5fc3124c
msatoh <msatoh@lily.ocn.ne.jp>
2020-04-19 21:38:24 +0900

    オブジェクト化

--- puz.py
+++ puz.py
@@ -42,59 +42,63 @@ class Mouse(Game):
 		self.canvas_height=canvas_height
 		self.canvas_width=canvas_width
 		self.white_canvas_array=white_canvas_array
+
+	def slide_pic(self,direction,x,y):
+		if direction=="right":
+			for i in range(1,21,1):
+				for j in range(1,20,1):
+					self.buf[i-1][j]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][self.canvas_width-20+j]
+			for i in range(1,21,1,):
+				for j in range(self.canvas_width,20,-1):
+					self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-1]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-20]
+			for i in range(1,21,1):
+				for j in range(1,20,1):
+					self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j]=self.buf[i-1][j]
+		elif direction=="left":
+			for i in range(1,21,1):
+				for j in range(1,20,1):
+					self.buf[i-1][j]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j]
+			for i in range(1,21,1,):
+				for j in range(20,self.canvas_width,1):
+					self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-20]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-1]
+			for i in range(1,21,1):
+				for j in range(1,20,1):
+					self.white_canvas_array[math.floor((y-1)/20)*20+i-1][self.canvas_width-20+j]=self.buf[i-1][j]
+		elif direction=="up":
+			for i in range(1,20,1):
+				for j in range(0,20,1):
+					self.buf[i][j]=self.white_canvas_array[i][math.floor((x-1)/20)*20+j-1]
+			for i in range(0,self.canvas_height-20,1,):
+				for j in range(0,20,1):
+					self.white_canvas_array[i][math.floor((x-1)/20)*20+j-1]=self.white_canvas_array[i+20][math.floor((x-1)/20)*20+j-1]
+			for i in range(1,20,1):
+				for j in range(0,20,1):
+					self.white_canvas_array[self.canvas_height-20+i][math.floor((x-1)/20)*20+j-1]=self.buf[i][j]
+		elif direction=="down":
+			for i in range(1,20,1):
+				for j in range(0,20,1):
+					self.buf[i][j]=self.white_canvas_array[self.canvas_height-20+i][math.floor((x-1)/20)*20+j-1]
+			for i in range(self.canvas_height-20,1,-1,):
+				for j in range(0,20,1):
+					self.white_canvas_array[i+19][math.floor((x-1)/20)*20+j-1]=self.white_canvas_array[i-1][math.floor((x-1)/20)*20+j-1]
+			for i in range(1,20,1):
+				for j in range(0,20,1):
+					self.white_canvas_array[i][math.floor((x-1)/20)*20+j-1]=self.buf[i][j]			
+		self.hold=True		
+
 	def mouse_event(self,event,x,y,flags,param):
 		if event==cv2.EVENT_LBUTTONDOWN:
 			self.x_hold=x
 			self.y_hold=y
-		elif event==cv2.EVENT_MOUSEMOVE and self.x_hold!=0 and self.y_hold!=0 and self.hold==False:
+		elif event==cv2.EVENT_MOUSEMOVE and self.x_hold!=0 and self.y_hold!=0 and self.hold==False: #マウスの左ボタンが押されたまま動いた時
 			if flags==cv2.EVENT_FLAG_LBUTTON:
 				if x-self.x_hold>10:
-					#print("→")
-					for i in range(1,21,1):
-						for j in range(1,20,1):
-							self.buf[i-1][j]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][self.canvas_width-20+j]
-					for i in range(1,21,1,):
-						for j in range(self.canvas_width,20,-1):
-							self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-1]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-20]
-					for i in range(1,21,1):
-						for j in range(1,20,1):
-							self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j]=self.buf[i-1][j]
-					self.hold=True
+					self.slide_pic("right",x,y)
 				elif x-self.x_hold<-10:
-					#print("←")
-					for i in range(1,21,1):
-						for j in range(1,20,1):
-							self.buf[i-1][j]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j]
-					for i in range(1,21,1,):
-						for j in range(20,self.canvas_width,1):
-							self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-20]=self.white_canvas_array[math.floor((y-1)/20)*20+i-1][j-1]
-					for i in range(1,21,1):
-						for j in range(1,20,1):
-							self.white_canvas_array[math.floor((y-1)/20)*20+i-1][self.canvas_width-20+j]=self.buf[i-1][j]
-					self.hold=True
+					self.slide_pic("left",x,y)
 				elif y-self.y_hold<-10:
-					#print("↑")
-					for i in range(1,20,1):
-						for j in range(0,20,1):
-							self.buf[i][j]=self.white_canvas_array[i][math.floor((x-1)/20)*20+j-1]
-					for i in range(0,self.canvas_height-20,1,):
-						for j in range(0,20,1):
-							self.white_canvas_array[i][math.floor((x-1)/20)*20+j-1]=self.white_canvas_array[i+20][math.floor((x-1)/20)*20+j-1]
-					for i in range(1,20,1):
-						for j in range(0,20,1):
-							self.white_canvas_array[self.canvas_height-20+i][math.floor((x-1)/20)*20+j-1]=self.buf[i][j]
-					self.hold=True
+					self.slide_pic("up",x,y)
 				elif y-self.y_hold>10:
-					#print("↓")
-					for i in range(1,20,1):
-						for j in range(0,20,1):
-							self.buf[i][j]=self.white_canvas_array[self.canvas_height-20+i][math.floor((x-1)/20)*20+j-1]
-					for i in range(self.canvas_height-20,1,-1,):
-						for j in range(0,20,1):
-							self.white_canvas_array[i+19][math.floor((x-1)/20)*20+j-1]=self.white_canvas_array[i-1][math.floor((x-1)/20)*20+j-1]
-					for i in range(1,20,1):
-						for j in range(0,20,1):
-							self.white_canvas_array[i][math.floor((x-1)/20)*20+j-1]=self.buf[i][j]
-					self.hold=True
+					self.slide_pic("down",x,y)
 		elif event==cv2.EVENT_LBUTTONUP:
 			self.hold=False
