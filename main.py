9f5539f0ccd2b2a686bf0f1e8e790589f79fdc4d
msatoh <msatoh@lily.ocn.ne.jp>
2020-04-22 22:26:03 +0900

    ファイル追加し忘れ

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import sys,tkinter,pdb,os
+import sys,tkinter,pdb,os,cv2
 from tkinter import filedialog
 import puzl
 
@@ -7,8 +7,23 @@ root=tkinter.Tk()
 root.title("karakuri")
 root.geometry("600x400")
 
+def helper(event):
+	cap = cv2.VideoCapture("mihon.mp4")
+	if not cap.isOpened():
+		sys.exit()
+	while True:
+		ret, frame = cap.read()
+		if ret:
+			cv2.imshow("操作方法", frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		elif cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	cv2.destroyWindow("操作方法")
+
 def select_pic(event):
-	file_type=[(u"画像","*.jpg")]
+	file_type=[("画像ファイル","*.jpg"),("画像ファイル","*.png"),("画像ファイル","*.bmp")]
 	if os.name=="posix":
 		directory="/home/"
 	elif os.name=="nt":
@@ -16,12 +31,21 @@ def select_pic(event):
 	#askopenfilename 一つのファイルを選択する。
 	filename=filedialog.askopenfilename(filetypes=file_type,initialdir=directory) 
 	root.destroy()
+	if not(len(filename)==0): 
+		start=puzl.Game(filename)
+		start.maingame()
 
-	start=puz.Game(filename)
-	start.maingame()
+##main##
 
-button=tkinter.Button(text="画像を選ぶ")
-button.bind("<Button-1>",select_pic)
-button.place(x=300,y=200)
+label = tkinter.Label(root, text="からくり(仮)", font=("",20))
+label.pack()
+
+b_sel_pic=tkinter.Button(text="画像を選ぶ")
+b_sel_pic.bind("<Button-1>",select_pic)
+b_sel_pic.place(x=300,y=200)
+
+b_help=tkinter.Button(text="操作方法")
+b_help.bind("<Button-1>",helper)
+b_help.place(x=300,y=250)
 
 root.mainloop()
