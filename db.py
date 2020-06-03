b6d2b9d5ed846903a1dac918a15b29312241e709
msatoh <msatoh@hotmail.co.jp>
2020-06-03 19:22:20 +0900

    平文ファイルレス化

--- db.py
+++ db.py
@@ -1,57 +1,64 @@
 #coding: UTF-8
-import sys,csv,pdb,cryptography
+import sys,io,csv,pdb,cryptography
 from cryptography.fernet import Fernet
 
 key=b"23456789012345678901234azZa+216549780219021="
 
-def crypt_csv():
-	with open("src/rank.csv","rb") as f:
-		data=f.read()
-		fernet=Fernet(key)
-		encrypted=fernet.encrypt(data)
+def crypt_csv(list):
+	data=list.encode()
+	fernet=Fernet(key)
+	encrypted=fernet.encrypt(data)
 	with open("src/rank.csv","wb") as f:
 		f.write(encrypted)
 
 def dec_csv():
 	with open("src/rank.csv","rb") as f:
 		data=f.read()
+		data.decode()
 		if data==b"":
-			return -1
+			return data
 		fernet=Fernet(key)
-		decrypted=fernet.decrypt(data)		
-	with open("src/rank.csv","wb") as f:
-		f.write(decrypted)
-	return 0
+		return fernet.decrypt(data)
 
 def fileoc(name,score):
-	dec_csv()
-	with open("src/rank.csv") as f:
-		reader=csv.reader(f)
-		# for row in reader:
-		# 	print("row: ",row)
-		l=[row for row in reader]
-		print("l: ",l)
+	reader=dec_csv()
+	reader.decode()
+	# for row in reader:
+	# 	print("row: ",row)
+	l=[row for row in reader]
+	print("l: ",l)
 		
-		pos=0#初期化
-		if not(score=="-1"):
-			for cnt in l:
-				if int(l[pos][1])<int(score):
-					break
-				pos+=1
+	pos=0#初期化
+	if not(score=="-1"):#-1:ランキング表示時。-1以外:スコア書き込み
+		for cnt in l:
+			if int(l[pos][1])<int(score):
+				break
+			pos+=1
 
-	if not(score=="-1"):	#-1:ゲーム終了時、スコア書き込み
-		with open("src/rank.csv",mode="w") as f:
-			writer=csv.writer(f)
-			add=[0]*2
-			add[0]=name
-			add[1]=score
-			adder=[add]
-			l.insert(pos-1,add)
-			writer.writerows(l[0:9])
-			#f.writelines(add)
-	crypt_csv()
+		add=[0]*2
+		add[0]=name
+		add[1]=score
+		adder=[add]
+		l.insert(pos-1,add)
+		# in_put=",".join(l[0:9])
+		# in_put.encode()
+
+		with io.StringIO() as f:
+			sys.stdout=f
+			print(l)
+			in_put=f.getvalue()
+			in_put.encode()
+			sys.stdout=sys.__stdout__
+		# i=0
+		# j=0
+		# for rnk in l:
+		# 	for elm in rnk:
+		# 		in_put[i][j]=elm.encode()
+		# 		j+=1
+		# 	i+=1
+		crypt_csv(in_put)
 
 	return l
 
 #testbench#
-fileoc(sys.argv[1],sys.argv[2])
+#fileoc(sys.argv[1],sys.argv[2])
